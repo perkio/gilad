57c1415a2d56c70bf15b9fa7b4ec594c8a78b043
amit <amitpp@gmail.com>
2024-11-01 11:54:45 +0200

    fix: admin

--- src/app/is-admin.tsx
+++ src/app/is-admin.tsx
@@ -1,3 +1,6 @@
 // TODO: migrate db schema
 
-export const isAdmin = async (id: string) => ["cm2yiq1fy00007g8tk1261qkd"].includes(id.toString());
+export const isAdmin = async (id: string) => [
+    "cm2yiq1fy00007g8tk1261qkd",
+    "cm2yk49f300003t2r4yplkgvc",
+].includes(id.toString());
