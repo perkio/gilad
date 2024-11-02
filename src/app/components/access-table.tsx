a7764e6edb9ec813d69fca718f214b78b70061b3
amit <amitpp@gmail.com>
2024-11-02 12:27:31 +0200

    fix: placeholder

--- src/app/components/access-table.tsx
+++ src/app/components/access-table.tsx
@@ -13,9 +13,9 @@ export function AccessTable({ results }: { results: Prisma.PromiseReturnType<typ
         <tr key={id}>
             <td data-label="אימייל">{email}</td>
             <td data-label="שם">{info ? `${info.firstName} ${info.lastName}` : name}</td>
-            <td data-label="בניין">{info ? `הגלעד ${info.building}` : ' '}</td>
-            <td data-label="דירה">{info ? `${info.apartment}` : ' '}</td>
-            <td data-label="טלפון">{info ? `${info.phonenumber}` : ' '}</td>
+            <td data-label="בניין">{info ? `הגלעד ${info.building}` : '-'}</td>
+            <td data-label="דירה">{info ? `${info.apartment}` : '-'}</td>
+            <td data-label="טלפון">{info ? `${info.phonenumber}` : '-'}</td>
             <td data-label="הרשאות">{gates_access.length ? gates_access.map(g => g.gates.name).join(", ") : "אין"}</td>
             <td data-label="פעולות"><button className="btn" onClick={() => router.push(`/admin/user/${id}`)}>ערוך</button></td>
         </tr>
