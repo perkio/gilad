d19c26f302d1cc258afc8d136d8a4e4e6a73e663
amit <amitpp@gmail.com>
2024-11-19 18:56:28 +0200

    fix: csp header generation

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,78 @@
 import withSerwistInit from "@serwist/next";
 
-// used by @vercel/speed-insights
-const vercelScriptsSrc = "https://va.vercel-scripts.com";
-const vercelLiveSrc = "https://vercel.live";
+/**
+ * @typedef {Object} SecurityPolicyEntry
+ * @property {string[]} [default-src] - Default fallback for fetch directives
+ * @property {string[]} [script-src] - Valid sources for JavaScript
+ * @property {string[]} [connect-src] - Valid targets for fetch, WebSocket, etc
+ * @property {string[]} [style-src] - Valid sources for stylesheets
+ * @property {string[]} [img-src] - Valid sources for images
+ * @property {string[]} [font-src] - Valid sources for fonts
+ * @property {string[]} [object-src] - Valid sources for plugins
+ * @property {string[]} [media-src] - Valid sources for media (audio/video)
+ * @property {string[]} [frame-src] - Valid sources for frames
+ * @property {string[]} [worker-src] - Valid sources for web workers
+ * @property {string[]} [manifest-src] - Valid sources for manifest files
+ * @property {string[]} [frame-ancestors] - Valid sources for frames
+ * @property {string[]} [base-uri] - Valid sources for base URIs
+ * @property {string[]} [form-action] - Valid sources for form actions
+ * @property {string[]} [report-uri] - Valid sources for report URIs
+ */
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' ${vercelLiveSrc} ${vercelScriptsSrc};
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
+/**
+ * Merges multiple security policies into a single policy string.
+ * @param {SecurityPolicyEntry[]} policies - Array of security policies
+ * @returns {string} - Combined security policy string
+ */
+function generateCSPHeader(policies) {
+  const combined = policies.reduce((combined, policy) => {
+    Object.keys(policy).forEach((directive) => {
+      const sources = Array.from(
+        new Set([...(combined[directive] ?? []), ...policy[directive]])
+      );
+      combined[directive] = sources;
+    });
+
+    return combined;
+  }, {});
+
+  const baseDirectives = Object.entries(combined).map(
+    ([directive, sources]) => `${directive} ${sources.sort().join(" ")}`
+  );
+
+  return [...baseDirectives, "upgrade-insecure-requests"].join("; ");
+}
+
+/** @type {SecurityPolicyEntry} */
+const defaultPolicy = {
+  "default-src": ["'self'"],
+  "script-src": ["'self'", "'unsafe-eval'", "'unsafe-inline'"],
+  "connect-src": ["'self'"],
+  "style-src": ["'self'", "'unsafe-inline'"],
+  "img-src": ["'self'", "data:", "blob:"],
+  "font-src": ["'self'"],
+  "manifest-src": ["'self'"],
+  "base-uri": ["'self'"],
+  "form-action": ["'self'"],
+  "frame-ancestors": ["'none'"],
+};
+
+const vercelSpeedPolicy = {
+  "script-src": ["https://va.vercel-scripts.com"]
+}
+
+const vercelLivePolicy = {
+  "connect-src": [
+    "https://vercel.live",
+    "https://*.pusher.com",
+    "wss://*.pusher.com",
+  ],
+  "img-src": ["https://vercel.com"],
+  "script-src": ["https://vercel.live"],
+  "style-src": ["https://vercel.live"],
+  "font-src": ["https://vercel.live"],
+  "frame-src": ["https://vercel.live"],
+};
 
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
@@ -41,7 +98,11 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
+            value: generateCSPHeader([
+              defaultPolicy,
+              vercelLivePolicy,
+              vercelSpeedPolicy,
+            ]),
           },
         ]
       }
